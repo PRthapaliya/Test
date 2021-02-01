5cab90d10b7f04e3525048e8fe2711925f4f0a8d
Puspa Raj Thapaliya <32583909+Zeroth9845@users.noreply.github.com>
2021-02-01 00:00:10 -0600

    New files created

---
+++ ezpayit/ezpayit/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ezpayit
+//
+//  Created by Senior Design on 1/31/21.
+//  Copyright © 2021 Senior Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
